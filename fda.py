3e0229bef03a01ef26df46d3eb744bcc95112363
Tavonga Mudzana <4138@longroad.ac.uk>
2014-11-04 07:53:31 +0000

    sffff

---
+++ fda.py
@@ -0,0 +1,14 @@
+freq=int(input("please how many numbers you want to average: "))
+counter=0
+
+while counter<freq:
+         number=int(input("enter the number: "))
+         total=number+1
+         average=total/freq
+         counter = counter + 1
+print(average)
+  
+         
+         
+         
+         
---
+++ itteration_class_ex_rev 5py.py
@@ -0,0 +1,16 @@
+#Tavonga Mudzana
+#11/2014
+#itteration_class_rev_ex5
+ 
+        
+for count in range (1,20,2.2):
+    print
+
+        
+        
+
+    
+
+        
+
+
