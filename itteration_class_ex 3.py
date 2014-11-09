70da29e6e8e4d1a23e0fa04431e8bf3f95897587
Tavonga Mudzana <4138@longroad.ac.uk>
2014-11-09 23:17:21 +0000

    ad

--- itteration_class_ex 3.py
+++ itteration_class_ex 3.py
@@ -3,12 +3,12 @@
 #itteration_class_ex_rev 4
 freq=int(input("please enter how many number you wnat to average: "))
 count=0
+total=0
 
-while count<freq:
+for count in range (freq):
     number=int(input("please enter the number: "))
-    total=number+0
-    average=total/freq
+    total=total+number
     count=count+1
-print(total)
+    average=total/count
 print(average)    
-
+  
---
+++ itteration_class_ex_rev 2.py
@@ -0,0 +1,7 @@
+counter=0
+message=input("please enter the message: ")
+freq=int(input("please enter howw many times you want to  print the message: "))
+
+while counter<freq+1:
+    print(message)
+    counter=counter+1
